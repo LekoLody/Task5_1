663598429d5c213835b83bb0231154f8a16cfee4
LekoLody <96385791+LekoLody@users.noreply.github.com>
2021-12-20 14:10:51 +0400

    Task 5.1 from module 5.3

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Elevator elevator = new Elevator(-3, 18);
+        while (true) {
+            System.out.print("Введите номер этажа: ");
+            int floor = new Scanner(System.in).nextInt();
+            elevator.move(floor);
+            System.out.println("Current Floor - " + elevator.getCurrentFloor());
+        }
+    }
+}
